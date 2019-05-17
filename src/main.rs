7473a926db0b6c62942a39d313b2b2118da03835
Kohei Taniguchi <kohei@chaintope.com>
2019-05-18 00:28:52 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+extern crate bitcoin;
+extern crate secp256k1;
+
+use bitcoin::{PrivateKey, Address};
+use secp256k1::{self, Secp256k1};
+
 fn main() {
-    println!("Hello, world!");
+    let private_key = PrivateKey::from_wif("L5PXRT9b9KSinEVzcm8pNZ42Bd4guarPsRS2vFp1FMYJEVFgM6Gr").unwrap();
+
+    let secp = Secp256k1::new();
+    let address = Address::p2pkh(&private_key.public_key(&secp), private_key.network);
+    println!("{}", address);
+
 }
 
 
