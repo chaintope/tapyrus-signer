c21fe66b9258a6d529510b2d0a5ffa9c6578ecc6
Kohei Taniguchi <kohei@chaintope.com>
2019-06-03 16:23:32 +0900

    Move sign test to the place it should be.

--- src/process_master_round.rs
+++ src/process_master_round.rs
@@ -33,30 +33,3 @@ pub fn process_master_round() -> Result<(), Error> {
 
     Ok(())
 }
-
-#[cfg(test)]
-mod test {
-    use base64;
-    use crate::test_helper::{TestKeys, get_block};
-    use super::*;
-
-    #[test]
-    fn sign_test() {
-        let private_key = TestKeys::new().key[0];
-        let block = get_block();
-        let block_hash = block.hash().unwrap();
-
-        let sig = sign(&private_key, &block_hash);
-
-        assert_eq!("MEUCIQDRTksobD+H7H46+EXJhsZ7CWSIZcqohndyAFYkEe6YvgIgWwzqhQr/IHrX+RU+CliF35tFzasfaXINrhWfdqErOok=",
-                   base64::encode(&sig.serialize_der()));
-
-        // check verifiable
-        let secp = Secp256k1::new();
-        let verify = Secp256k1::verification_only();
-        let message = Message::from_slice(&(block_hash.borrow_inner())[..]).unwrap();
-        let public_key = private_key.public_key(&secp).key;
-        assert!(verify.verify(&message, &sig, &public_key).is_ok());
-
-    }
-}
--- src/sign.rs
+++ src/sign.rs
@@ -7,3 +7,29 @@ pub fn sign(private_key: &PrivateKey, hash: &BlockHash) -> Signature {
     let message = Message::from_slice(&(hash.borrow_inner())[..]).unwrap();
     sign.sign(&message, &(private_key.key))
 }
+
+#[cfg(test)]
+mod test {
+    use base64;
+    use crate::test_helper::{TestKeys, get_block};
+    use super::*;
+
+    #[test]
+    fn sign_test() {
+        let private_key = TestKeys::new().key[0];
+        let block = get_block();
+        let block_hash = block.hash().unwrap();
+
+        let sig = sign(&private_key, &block_hash);
+
+        assert_eq!("MEUCIQDRTksobD+H7H46+EXJhsZ7CWSIZcqohndyAFYkEe6YvgIgWwzqhQr/IHrX+RU+CliF35tFzasfaXINrhWfdqErOok=",
+                   base64::encode(&sig.serialize_der()));
+
+        // check verifiable
+        let secp = Secp256k1::new();
+        let verify = Secp256k1::verification_only();
+        let message = Message::from_slice(&(block_hash.borrow_inner())[..]).unwrap();
+        let public_key = private_key.public_key(&secp).key;
+        assert!(verify.verify(&message, &sig, &public_key).is_ok());
+    }
+}
