9aac64e2ed04b46c3a2f5c0704835436c6cbac33
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-04-23 19:08:11 +0900

    Add length of xfield

--- src/blockdata.rs
+++ src/blockdata.rs
@@ -3,7 +3,9 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 use crate::serialize::HexStrVisitor;
+use bitcoin::consensus::encode::serialize;
 use bitcoin::PublicKey;
+use bitcoin::VarInt;
 use bitcoin_hashes::{sha256d, Hash};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::fmt::Debug;
@@ -94,7 +96,10 @@ impl Block {
     /// xfieldType: 1
     /// xfield: variable
     pub fn get_header_without_proof(&self) -> &[u8] {
-        let position = Self::XFIELD_POSITION + self.get_aggregated_public_key_length();
+        let position = match self.get_xfield_length() {
+            None => Self::XFIELD_POSITION,
+            Some(i) => Self::XFIELD_POSITION + i.len() + i.0 as usize,
+        };
         &self.0[..position]
     }
 
@@ -116,7 +121,10 @@ impl Block {
     }
 
     pub fn add_proof(&self, proof: Vec<u8>) -> Block {
-        let position = Self::XFIELD_POSITION + self.get_aggregated_public_key_length();
+        let position = match self.get_xfield_length() {
+            None => Self::XFIELD_POSITION,
+            Some(i) => Self::XFIELD_POSITION + i.len() + i.0 as usize,
+        };
         let (header, txs) = self.payload().split_at(position);
         let new_payload = [header, &proof[..], &txs[1..]].concat();
         Block(new_payload)
@@ -125,23 +133,73 @@ impl Block {
     pub fn add_aggregated_public_key(&self, aggregated_public_key: PublicKey) -> Block {
         let (header, rest) = self.payload().split_at(Self::XFIELD_POSITION - 1);
         let bytes = aggregated_public_key.to_bytes();
-        let new_payload = [header, &[0x01], &bytes[..], &rest[1..]].concat();
+        let size = serialize(&VarInt(bytes.len() as u64));
+        let new_payload = [header, &[0x01], &size, &bytes[..], &rest[1..]].concat();
         Block(new_payload)
     }
 
     pub fn get_aggregated_public_key(&self) -> Option<PublicKey> {
-        let len = self.get_aggregated_public_key_length();
-        let bytes = &self.payload()[Self::XFIELD_POSITION..Self::XFIELD_POSITION + len];
-        PublicKey::from_slice(bytes).ok()
+        match self.0[Self::XFIELD_POSITION - 1] {
+            1 => {
+                let len = self.get_xfield_length().expect("can not get xfield size");
+                let bytes = &self.payload()[Self::XFIELD_POSITION + len.len()
+                    ..Self::XFIELD_POSITION + len.len() + len.0 as usize];
+                PublicKey::from_slice(bytes).ok()
+            }
+            _ => None,
+        }
     }
 
-    /// the length of aggregated public key.
+    /// the length of xfield.
+    /// return 0  if xfieldType is None.
     /// return 33 if xfieldType is AggregatePublicKey.
-    /// return 0 otherwise
-    fn get_aggregated_public_key_length(&self) -> usize {
+    /// return value of the first byte if xfieldType is unknown,
+    /// this assumes that the leading of the new xfieldType added in the future is the length of the xfield value.
+    fn get_xfield_length(&self) -> Option<VarInt> {
         match self.0[Self::XFIELD_POSITION - 1] {
-            1 => 33,
-            _ => 0,
+            0 => None,
+            _ => {
+                let n = self.0[Self::XFIELD_POSITION];
+                match n {
+                    0xFF => {
+                        let mut slice = [0u8; 8];
+                        slice.copy_from_slice(
+                            &self.0[Self::XFIELD_POSITION + 1..Self::XFIELD_POSITION + 9],
+                        );
+                        let x = u64::from_le_bytes(slice);
+                        if x < 0x100000000 {
+                            None
+                        } else {
+                            Some(VarInt(x))
+                        }
+                    }
+                    0xFE => {
+                        let mut slice = [0u8; 4];
+                        slice.copy_from_slice(
+                            &self.0[Self::XFIELD_POSITION + 1..Self::XFIELD_POSITION + 5],
+                        );
+                        let x = u32::from_le_bytes(slice);
+                        if x < 0x10000 {
+                            None
+                        } else {
+                            Some(VarInt(x as u64))
+                        }
+                    }
+                    0xFD => {
+                        let mut slice = [0u8; 2];
+                        slice.copy_from_slice(
+                            &self.0[Self::XFIELD_POSITION + 1..Self::XFIELD_POSITION + 3],
+                        );
+                        let x = u16::from_le_bytes(slice);
+                        if x < 0xFD {
+                            None
+                        } else {
+                            Some(VarInt(x as u64))
+                        }
+                    }
+                    n => Some(VarInt(n as u64)),
+                }
+            }
         }
     }
 }
@@ -179,10 +237,11 @@ mod tests {
     use std::str::FromStr;
 
     const TEST_BLOCK: &str = "010000000000000000000000000000000000000000000000000000000000000000000000c1457ff3e5c527e69858108edf0ff1f49eea9c58d8d37300a164b3b4f8c8c7cef1a2e72770d547feae29f2dd40123a97c580d44fd4493de072416d53331997617b96f05d00403a4c09253c7b583e5260074380c9b99b895f938e37799d326ded984fb707e91fa4df2e0524a4ccf5fe224945b4fb94784b411a760eb730d95402d3383dd7ffdc01010000000100000000000000000000000000000000000000000000000000000000000000000000000022210366262690cbdf648132ce0c088962c6361112582364ede120f3780ab73438fc4bffffffff0100f2052a010000002776a9226d70757956774d32596a454d755a4b72687463526b614a787062715447417346484688ac00000000";
-    const TEST_BLOCK2: &str = "010000000000000000000000000000000000000000000000000000000000000000000000e7c526d0125538b13a50b06465fb8b72120be13fb1142e93aba2aabb2a4f369826c18219f76e4d0ebddbaa9b744837c2ac65b347673695a23c3cc1a2be4141e1427d735e01025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3403a4c09253c7b583e5260074380c9b99b895f938e37799d326ded984fb707e91fa4df2e0524a4ccf5fe224945b4fb94784b411a760eb730d95402d3383dd7ffdc0101000000010000000000000000000000000000000000000000000000000000000000000000000000002221025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3ffffffff0100f2052a010000001976a914834e0737cdb9008db614cd95ec98824e952e3dc588ac00000000";
-    const TEST_BLOCK_WITH_PUBKEY: &str = "010000000000000000000000000000000000000000000000000000000000000000000000e7c526d0125538b13a50b06465fb8b72120be13fb1142e93aba2aabb2a4f369826c18219f76e4d0ebddbaa9b744837c2ac65b347673695a23c3cc1a2be4141e1427d735e01025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3000101000000010000000000000000000000000000000000000000000000000000000000000000000000002221025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3ffffffff0100f2052a010000001976a914834e0737cdb9008db614cd95ec98824e952e3dc588ac00000000";
+    const TEST_BLOCK2: &str = "010000000000000000000000000000000000000000000000000000000000000000000000e7c526d0125538b13a50b06465fb8b72120be13fb1142e93aba2aabb2a4f369826c18219f76e4d0ebddbaa9b744837c2ac65b347673695a23c3cc1a2be4141e1427d735e0121025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3403a4c09253c7b583e5260074380c9b99b895f938e37799d326ded984fb707e91fa4df2e0524a4ccf5fe224945b4fb94784b411a760eb730d95402d3383dd7ffdc0101000000010000000000000000000000000000000000000000000000000000000000000000000000002221025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3ffffffff0100f2052a010000001976a914834e0737cdb9008db614cd95ec98824e952e3dc588ac00000000";
+    const TEST_BLOCK_WITH_PUBKEY: &str = "010000000000000000000000000000000000000000000000000000000000000000000000e7c526d0125538b13a50b06465fb8b72120be13fb1142e93aba2aabb2a4f369826c18219f76e4d0ebddbaa9b744837c2ac65b347673695a23c3cc1a2be4141e1427d735e0121025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3000101000000010000000000000000000000000000000000000000000000000000000000000000000000002221025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3ffffffff0100f2052a010000001976a914834e0737cdb9008db614cd95ec98824e952e3dc588ac00000000";
     const TEST_BLOCK_WITHOUT_PUBKEY: &str = "010000000000000000000000000000000000000000000000000000000000000000000000e7c526d0125538b13a50b06465fb8b72120be13fb1142e93aba2aabb2a4f369826c18219f76e4d0ebddbaa9b744837c2ac65b347673695a23c3cc1a2be4141e1427d735e00000101000000010000000000000000000000000000000000000000000000000000000000000000000000002221025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3ffffffff0100f2052a010000001976a914834e0737cdb9008db614cd95ec98824e952e3dc588ac00000000";
     const TEST_BLOCK_WITHOUT_PROOF: &str = "010000000000000000000000000000000000000000000000000000000000000000000000c1457ff3e5c527e69858108edf0ff1f49eea9c58d8d37300a164b3b4f8c8c7cef1a2e72770d547feae29f2dd40123a97c580d44fd4493de072416d53331997617b96f05d000001010000000100000000000000000000000000000000000000000000000000000000000000000000000022210366262690cbdf648132ce0c088962c6361112582364ede120f3780ab73438fc4bffffffff0100f2052a010000002776a9226d70757956774d32596a454d755a4b72687463526b614a787062715447417346484688ac00000000";
+    const TEST_BLOCK_WITH_UNKNOWN_XFIELD: &str = "010000000000000000000000000000000000000000000000000000000000000000000000e7c526d0125538b13a50b06465fb8b72120be13fb1142e93aba2aabb2a4f369826c18219f76e4d0ebddbaa9b744837c2ac65b347673695a23c3cc1a2be4141e1427d735efffd2602ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff000101000000010000000000000000000000000000000000000000000000000000000000000000000000002221025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3ffffffff0100f2052a010000001976a914834e0737cdb9008db614cd95ec98824e952e3dc588ac00000000";
 
     fn test_block() -> Block {
         let raw_block = hex::decode(TEST_BLOCK).unwrap();
@@ -209,6 +268,11 @@ mod tests {
         Block(raw_block)
     }
 
+    fn test_block_with_unknown_xfield() -> Block {
+        let raw_block = hex::decode(TEST_BLOCK_WITH_UNKNOWN_XFIELD).unwrap();
+        Block(raw_block)
+    }
+
     #[test]
     fn test_get_header_without_proof() {
         let block = test_block();
@@ -219,7 +283,7 @@ mod tests {
         assert_eq!(block.get_header_without_proof(), &raw_expect[..]);
 
         let block = test_block_with_pubkey();
-        let hex_expect = "010000000000000000000000000000000000000000000000000000000000000000000000e7c526d0125538b13a50b06465fb8b72120be13fb1142e93aba2aabb2a4f369826c18219f76e4d0ebddbaa9b744837c2ac65b347673695a23c3cc1a2be4141e1427d735e01025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3";
+        let hex_expect = "010000000000000000000000000000000000000000000000000000000000000000000000e7c526d0125538b13a50b06465fb8b72120be13fb1142e93aba2aabb2a4f369826c18219f76e4d0ebddbaa9b744837c2ac65b347673695a23c3cc1a2be4141e1427d735e0121025700236c2890233592fcef262f4520d22af9160e3d9705855140eb2aa06c35d3";
         let raw_expect = hex::decode(hex_expect).unwrap();
 
         assert_eq!(block.get_header_without_proof(), &raw_expect[..]);
@@ -284,5 +348,16 @@ mod tests {
         let json = serde_json::to_string(&block).unwrap();
         let deserialize_block: Block = serde_json::from_str(&json).unwrap();
         assert_eq!(deserialize_block, block);
+    }
+
+    #[test]
+    fn test_get_xfield_length() {
+        let block = test_block_with_pubkey();
+        let len = block.get_xfield_length().unwrap();
+        assert_eq!(len, VarInt(33));
+
+        let block = test_block_with_unknown_xfield();
+        let len = block.get_xfield_length().unwrap();
+        assert_eq!(len, VarInt(550));
     }
 }
