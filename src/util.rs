5df738e100e175136cec2c20aecae7ab66ece4a3
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-08-05 21:03:00 +0900

    Use jacobi function in rust-tapyrus

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,4 @@
-use curv::{BigInt, GE};
+use curv::GE;
 use std::convert::TryFrom;
 use std::os::raw::c_int;
 use std::sync::atomic::AtomicUsize;
@@ -9,48 +9,6 @@ pub fn sum_point(points: &Vec<GE>) -> GE {
     let head = iter.next().unwrap();
     let tail = iter;
     tail.fold(head.clone(), |acc, x| acc + x)
-}
-
-pub fn jacobi(a: &BigInt, n: &BigInt) -> i8 {
-    assert!(*n >= BigInt::from(3));
-    assert!(a < n);
-
-    if a.is_zero() {
-        return 0;
-    }
-    if *a == BigInt::from(1) {
-        return 1;
-    }
-
-    let mut a1: BigInt = a.clone();
-    let mut e = 0;
-    while a1.is_multiple_of(&BigInt::from(2)) {
-        a1 = a1 >> 1;
-        e += 1;
-    }
-    let mut s: i8 = if e & 1 == 0
-        || n.modulus(&BigInt::from(8)) == BigInt::from(1)
-        || n.modulus(&BigInt::from(8)) == BigInt::from(7)
-    {
-        1
-    } else if n.modulus(&BigInt::from(8)) == BigInt::from(3)
-        || n.modulus(&BigInt::from(8)) == BigInt::from(5)
-    {
-        -1
-    } else {
-        0
-    };
-    if n.modulus(&BigInt::from(4)) == BigInt::from(3)
-        && a1.modulus(&BigInt::from(4)) == BigInt::from(3)
-    {
-        s = -s
-    }
-
-    if a1 == BigInt::from(1) {
-        s
-    } else {
-        s * jacobi(&(n % a1.clone()), &a1.clone())
-    }
 }
 
 const STOP_SIGNALS: [usize; 6] = [
@@ -115,13 +73,6 @@ mod tests {
         let s6 = ECScalar::from(&BigInt::from(6));
         let p6 = GE::generator() * &s6;
         assert_eq!(sum, p6);
-    }
-
-    #[test]
-    fn test_jacobi() {
-        assert_eq!(jacobi(&BigInt::from(158), &BigInt::from(235)), -1);
-        assert_eq!(jacobi(&BigInt::from(5), &BigInt::from(12)), -1);
-        assert_eq!(jacobi(&BigInt::from(16), &BigInt::from(60)), 1);
     }
 
     #[test]
