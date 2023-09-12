bb6d5648b1e668303ef897a33a83f404281d7d6e
Navia Bheeman <navia@chaintope.com>
2023-09-12 11:50:53 +0530

    Add more comments, fix lint errors separate filename from the code by making it a separate constant

--- src/cli/setup/federation_change.rs
+++ src/cli/setup/federation_change.rs
@@ -1,9 +1,8 @@
-
 use crate::cli::setup::traits::Response;
 
 use crate::errors::Error;
 
-use clap::{App, Arg, ArgMatches, SubCommand, ArgGroup};
+use clap::{App, Arg, ArgGroup, ArgMatches, SubCommand};
 use std::fs;
 use std::fs::File;
 use std::io::Write;
@@ -12,7 +11,12 @@ use std::str::FromStr;
 use std::time::{Duration, UNIX_EPOCH};
 use tapyrus::blockdata::block::XField;
 
-///federation 
+///federation command is used to register/unregister federation change.
+/// When register is invoked it writes a new file with the given parameters.
+/// When unregister is invoked it removes the file with the given parameters.
+///
+/// FILE_NAME: federationchange_<timestamp>.dat"
+/// PATH: WRITE_DIR
 ///
 ///USAGE:
 ///federation [OPTIONS] <change> --height <height> <--aggregated-public-key <aggregated-public-key>|--max-block-size <max-block-size>>
@@ -29,19 +33,18 @@ use tapyrus::blockdata::block::XField;
 ///ARGS:
 ///    <change>    register/unregister federation change [possible values: register, unregister]
 ///
-/// 
-/// FILE_NAME: 
-/// 
-//TODO - change directory
+
+//TODO - change this directory to a runtime directory
 #[cfg(debug_assertions)]
 const WRITE_DIR: &str = "target/debug/data/";
 
 #[cfg(not(debug_assertions))]
 const WRITE_DIR: &str = "target/release/data/";
 
+const FILE_NAME: &str = "federationchange_";
 pub struct RegisterFederationChangeResponse {
-    xfield : XField,
-    height:  u32,
+    xfield: XField,
+    height: u32,
     timestamp: std::time::Duration,
 }
 
@@ -52,27 +55,30 @@ impl RegisterFederationChangeResponse {
             height: height,
             timestamp: match std::time::SystemTime::now().duration_since(UNIX_EPOCH) {
                 Ok(n) => n,
-                Err(_) => Duration::new(0, 0)
-            }
+                Err(_) => Duration::new(0, 0),
+            },
         }
     }
 }
 
 impl std::fmt::Display for RegisterFederationChangeResponse {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:>010}{}@{:?}", self.height, self.xfield, self.timestamp)
+        write!(
+            f,
+            "{:>010}{}@{}",
+            self.height,
+            self.xfield,
+            self.timestamp.as_micros()
+        )
     }
 }
 
 impl Response for RegisterFederationChangeResponse {}
 
-
 pub struct RegisterFederationChangeCommand {}
 
 impl<'a> RegisterFederationChangeCommand {
-
     pub fn execute(matches: &ArgMatches) -> Result<Box<dyn Response>, Error> {
-
         let change: &str = matches
             .value_of("change")
             .ok_or(Error::InvalidArgs("register/unregister".to_string()))?;
@@ -91,22 +97,22 @@ impl<'a> RegisterFederationChangeCommand {
         if height == 0 {
             return Err(Error::InvalidArgs(format!("Height must be greater than 0")));
         }
-        
+
         let xfield_public_key: XField = match matches.value_of("aggregated-public-key") {
-                Some(key) =>  match tapyrus::PublicKey::from_str(key) {
-                    Ok(public_key) => XField::AggregatePublicKey(public_key),
-                    Err(_) =>  XField::None,
-                }
-                None => XField::None,
-            };
+            Some(key) => match tapyrus::PublicKey::from_str(key) {
+                Ok(public_key) => XField::AggregatePublicKey(public_key),
+                Err(_) => XField::None,
+            },
+            None => XField::None,
+        };
 
         let xfield_max_block_size: XField = match matches.value_of("max-block-size") {
-                Some(s) => match s.parse::<u32>() {
-                    Ok(x) => XField::MaxBlockSize(x),
-                    Err(_) => XField::None,
-                }
-                None => XField::None,
-            };
+            Some(s) => match s.parse::<u32>() {
+                Ok(x) => XField::MaxBlockSize(x),
+                Err(_) => XField::None,
+            },
+            None => XField::None,
+        };
 
         let resp: RegisterFederationChangeResponse = match (&xfield_public_key, &xfield_max_block_size) {
             (XField::AggregatePublicKey(_), XField::None) =>
@@ -118,19 +124,22 @@ impl<'a> RegisterFederationChangeCommand {
 
         if register {
             RegisterFederationChangeCommand::register(resp)?;
-        }
-        else {
+        } else {
             RegisterFederationChangeCommand::unregister(resp)?;
         }
 
         //empty response
-        Ok(Box::new(RegisterFederationChangeResponse::new(XField::None, height)))
+        Ok(Box::new(RegisterFederationChangeResponse::new(
+            XField::None,
+            height,
+        )))
     }
 
-    pub fn register(resp:RegisterFederationChangeResponse) -> Result<(), Error>{
+    pub fn register(resp: RegisterFederationChangeResponse) -> Result<(), Error> {
         fs::create_dir_all(WRITE_DIR)?;
 
-        let file_path = Path::new(WRITE_DIR).join( format!("federationchange_{:?}.dat", resp.timestamp));
+        let file_path =
+            Path::new(WRITE_DIR).join(format!("{}{}.dat", FILE_NAME, resp.timestamp.as_micros()));
         let mut file = File::create(&file_path)?;
         file.write_all(format!("{}", resp).as_ref())?;
         file.flush()?;
@@ -138,54 +147,54 @@ impl<'a> RegisterFederationChangeCommand {
         Ok(())
     }
 
-    pub fn unregister(_resp:RegisterFederationChangeResponse) -> Result<(), Error>{
+    pub fn unregister(_resp: RegisterFederationChangeResponse) -> Result<(), Error> {
         //TODO: implement
         Ok(())
     }
 
     pub fn args<'b>() -> App<'a, 'b> {
-        SubCommand::with_name("federation").args(&[
-            Arg::with_name("change")
-                .required(true)
-                .takes_value(true)
-                .possible_values(&["register", "unregister"])
-                .help("register/unregister federation change"),
-            Arg::with_name("height")
-                .long("height")
-                .required(true)
-                .takes_value(true)
-                .display_order(99)
-                .help("block height from which the change can be made"),
-            Arg::with_name("aggregated-public-key")
-                .long("aggregated-public-key")
-                .takes_value(true)
-                .display_order(1)
-                .help("aggregated public key of the new federation"),
-            Arg::with_name("max-block-size")
-                .long("max-block-size")
-                .takes_value(true)
-                .display_order(2)
-                .help("max block size of the federation"),
-        ])
-        .group(ArgGroup::with_name("xfield")
-            .args(&["aggregated-public-key", "max-block-size"])
-            .required(true)
-            .multiple(false))
+        SubCommand::with_name("federation")
+            .args(&[
+                Arg::with_name("change")
+                    .required(true)
+                    .takes_value(true)
+                    .possible_values(&["register", "unregister"])
+                    .help("register/unregister federation change"),
+                Arg::with_name("height")
+                    .long("height")
+                    .required(true)
+                    .takes_value(true)
+                    .display_order(99)
+                    .help("block height from which the change can be made"),
+                Arg::with_name("aggregated-public-key")
+                    .long("aggregated-public-key")
+                    .takes_value(true)
+                    .display_order(1)
+                    .help("aggregated public key of the new federation"),
+                Arg::with_name("max-block-size")
+                    .long("max-block-size")
+                    .takes_value(true)
+                    .display_order(2)
+                    .help("max block size of the federation"),
+            ])
+            .group(
+                ArgGroup::with_name("xfield")
+                    .args(&["aggregated-public-key", "max-block-size"])
+                    .required(true)
+                    .multiple(false),
+            )
     }
 }
-
-
-
 
 #[cfg(test)]
 
 mod tests {
     use super::*;
-    use std::fs::DirEntry;
-    use std::process::Command;
     use std::fs;
-    use std::str;
+    use std::fs::DirEntry;
     use std::path::PathBuf;
+    use std::process::Command;
+    use std::str;
 
     #[cfg(debug_assertions)]
     const DIR: &str = "debug/";
@@ -193,43 +202,53 @@ mod tests {
     #[cfg(not(debug_assertions))]
     const DIR: &str = "release/";
 
-    fn check_file(content:String) {
+    fn check_file(content: String) {
         // Check that the file was created
         let contents = match fs::read_dir(WRITE_DIR) {
             Ok(contents) => contents,
-            Err(err) => { panic!("Error: {}", err); }
+            Err(err) => {
+                panic!("Error: {}", err);
+            }
         };
 
         let mut found = false;
         // As these tests run in parallel multiple files are created.
         // SO read all files before concluding failure
         for x in contents {
-            let entry:DirEntry = match x {
+            let entry: DirEntry = match x {
                 Ok(e) => e,
-                Err(err) => { panic!("Error: {}", err); }
+                Err(err) => {
+                    panic!("Error: {}", err);
+                }
             };
             let file_name = entry.file_name();
             let file_name_str = file_name.to_string_lossy().into_owned();
 
-            if file_name_str.starts_with("federationchange_") {
+            if file_name_str.starts_with(FILE_NAME) {
                 let file_path = PathBuf::from(WRITE_DIR).join(&file_name);
 
-                let file_contents =  match fs::read_to_string(&file_path){
+                let file_contents = match fs::read_to_string(&file_path) {
                     Ok(contents) => contents,
-                    Err(err) => {panic!("Error reading file: {}", err);}
+                    Err(err) => {
+                        panic!("Error reading file: {}", err);
+                    }
                 };
-                let line:String = match file_contents.parse() {
+                let line: String = match file_contents.parse() {
                     Ok(contents) => contents,
-                    Err(err) => {panic!("Error parsing: {}", err);}
+                    Err(err) => {
+                        panic!("Error parsing: {}", err);
+                    }
                 };
                 if line.contains(&content) {
-                    found = true; break;
+                    found = true;
+                    break;
                 }
                 continue;
-
             }
         }
-        if found { return; }
+        if found {
+            return;
+        }
         panic!("File not found");
     }
 
@@ -259,7 +278,10 @@ mod tests {
         ]);
         let response = RegisterFederationChangeCommand::execute(&matches);
         assert!(response.is_ok());
-        check_file("00000001000121033e6e1d4ae3e7e1bc2173e2af1f2f65c6284ea7c6478f2241784c77b0dff98e61".to_string());
+        check_file(
+            "00000001000121033e6e1d4ae3e7e1bc2173e2af1f2f65c6284ea7c6478f2241784c77b0dff98e61"
+                .to_string(),
+        );
     }
 
     #[test]
@@ -331,10 +353,8 @@ mod tests {
         );
     }
 
-
     #[test]
     fn test_execute_fail_register_both() {
-
         let output = Command::new(format!("target/{}/tapyrus-setup", DIR).to_string())
             .arg("federation")
             .arg("register")
@@ -347,13 +367,10 @@ mod tests {
             .output()
             .expect("failed to execute process");
 
-        // Check that the exit status indicates a failure
         assert!(!output.status.success());
-
-        // Optionally, you can check the output message as well
         let stderr_str = str::from_utf8(&output.stderr).unwrap();
-        assert!(stderr_str.contains("cannot be used with one or more of the other specified arguments"));
+        assert!(
+            stderr_str.contains("cannot be used with one or more of the other specified arguments")
+        );
     }
-
-
 }
