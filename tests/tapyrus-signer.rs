1d489f5aa2c6bb2af1bf9cf6595f6ec333c3b8eb
azuchi <azuchi@chaintope.com>
2025-01-07 16:14:47 +0900

    Fix missiong_docs error

--- tests/tapyrus-signer.rs
+++ tests/tapyrus-signer.rs
@@ -1,5 +1,7 @@
 // Copyright (c) 2019 Chaintope Inc.
 
+//! This is the documentation for the tapyrus-signer test module.
+//! It provides tests for the tapyrus-signer crate.
 #![deny(warnings, missing_docs)]
 
 extern crate tapyrus_signer;
